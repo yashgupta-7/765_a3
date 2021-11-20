feee1a666a3a5ee479e5d7e4f8983876ddbe5ed9
c-nikhil31 <88959361+c-nikhil31@users.noreply.github.com>
2021-11-20 23:23:07 +0530

    Update MyContract.sol

--- MyContract.sol
+++ MyContract.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.4.25 <7.0.0;
 
 contract MyContract{
-    uint public totSuccess = 0;
+    uint public totSuccess = 0; // count of successful transactions
     /*
     struct Queue {
         uint[] items;
@@ -27,10 +27,10 @@ contract MyContract{
         self.head++;
     }*/
 
-    struct User{
+    struct User{ // struct to define a user
         uint user_id;
     	string user_name;
-        mapping(uint=>uint) edge_list;
+        mapping(uint=>uint) edge_list; // connected users mapped to the corresponding balance
     }
     
     mapping(uint=>User) allUsers;
@@ -53,12 +53,12 @@ contract MyContract{
     mapping(uint=>uint) parent;
     mapping(uint=>bool) visited;
     
-    function dfs(uint source, uint amt) public{
+    function findPath(uint source, uint amt) public{ // bfs to find the shortest path
         visited[source] = true;
         for(uint i=0;i<100;i++){
             if(allUsers[source].edge_list[i]>amt && !visited[i]){
                 parent[i] = source;
-                dfs(i,amt);
+                findPath(i,amt);
             }
         }
     }
@@ -67,10 +67,12 @@ contract MyContract{
         /*Queue storage q;
         create(q,0);
         append(q,user_id_1);*/
+        // find shortest path with valid balances and execute transaction
+        
         for(uint j=0;j<100;j++) visited[j] = false;
         parent[user_id_2] = user_id_2;
-        dfs(user_id_1,amt);
-        
+        findPath(user_id_1,amt);
+        // iterate through parents to get to source user from destination user
         if(parent[user_id_2]!=user_id_2){
             uint temp = user_id_2;
             while(temp!=user_id_1){
@@ -83,7 +85,7 @@ contract MyContract{
         }
         else{flag=false;}
     }
-    
+    // function for debugging
     function checkFlag() public view returns (bool){
         return flag;
     }
@@ -91,7 +93,7 @@ contract MyContract{
     function getSucCount() public view returns (uint){
         return totSuccess;
     }
-    
+    // closes link - makes balance zero, so never usable
     function closeAcc(uint user_id_1, uint user_id_2) public{
         allUsers[user_id_1].edge_list[user_id_2] = 0;
         allUsers[user_id_2].edge_list[user_id_1] = 0;
