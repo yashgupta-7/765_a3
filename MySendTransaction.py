a30598a983827f47750d83eb185d947bdbec121e
yashgupta-7 <yashgupta@cse.iitb.ac.in>
2021-11-20 23:17:40 +0530

    final

--- MySendTransaction.py
+++ MySendTransaction.py
@@ -7,6 +7,8 @@ from solcx import compile_source
 import os
 
 contract_source_path = os.environ['HOME']+'/765_a3/MyContract.sol'
+logs = True
+grcpt = False
 
 def compile_source_file(file_path):
    with open(file_path, 'r') as f:
@@ -17,41 +19,48 @@ def getReceipt(tx_hash3):
     # receipt3 = w3.eth.getTransactionReceipt(tx_hash3)
     while True:
         try: 
-            # time.sleep(1)
+            time.sleep(0.1)
             receipt3 = w3.eth.getTransactionReceipt(tx_hash3)
             break
         except:
             continue
     receipt3 = w3.eth.getTransactionReceipt(tx_hash3)
-    # if receipt3 is not None:
-        # print("empty:{0}".format(receipt3['gasUsed']))
-        # print()
+    if receipt3 is not None and logs:
+        print("empty:{0}".format(receipt3['gasUsed']))
     return
 
-def registerUserTransaction(sort_contract, user_id):
-    print("Registering User:", user_id)
+def registerUserTransaction(sort_contract, user_id, gr=False):
+    if logs:
+        print("Registering User:", user_id)
     tx_hash = sort_contract.functions.registerUser(user_id, "YG").transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
-    # getReceipt(tx_hash)
+    if gr:
+        getReceipt(tx_hash)
     return tx_hash
 
 import numpy as np
-def createAccTransaction(sort_contract, user_id_1, user_id_2):
-    print("Creating Account between:", user_id_1, user_id_2)
+def createAccTransaction(sort_contract, user_id_1, user_id_2, gr=False):
+    if logs:
+        print("Creating Account between:", user_id_1, user_id_2)
     amt = int(np.random.exponential(10) * 0.5)
     tx_hash = sort_contract.functions.createAcc(user_id_1, user_id_2, amt).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
-    # getReceipt(tx_hash)
+    if gr:
+        getReceipt(tx_hash)
     return tx_hash
 
-def closeAccTransaction(sort_contract, user_id_1, user_id_2):
-    print("Closing Account between:", user_id_1, user_id_2)
+def closeAccTransaction(sort_contract, user_id_1, user_id_2, gr=False):
+    if logs:
+        print("Closing Account between:", user_id_1, user_id_2)
     tx_hash = sort_contract.functions.closeAcc(user_id_1, user_id_2).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
-    # getReceipt(tx_hash)
+    if gr:
+        getReceipt(tx_hash)
     return tx_hash
 
-def sendAmountTransaction(sort_contract, user_id_1, user_id_2, amt):
-    print("Attempt to send ", amt, " from ", user_id_1, " to ", user_id_2)
+def sendAmountTransaction(sort_contract, user_id_1, user_id_2, amt, gr=False):
+    if logs:
+        print("Attempt to send ", amt, " from ", user_id_1, " to ", user_id_2)
     tx_hash = sort_contract.functions.sendAmount(user_id_1, user_id_2, amt).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
-    # getReceipt(tx_hash)
+    if gr:
+        getReceipt(tx_hash)
     return tx_hash
 
 def getSucCountCall(sort_contract):
@@ -96,9 +105,10 @@ t = 0
 
 wait_list = []
 for i in range(N):
-    wait_list.append(registerUserTransaction(sort_contract, i))
-for wl in wait_list:
-    getReceipt(wl)
+    wait_list.append(registerUserTransaction(sort_contract, i, gr=grcpt))
+if not grcpt:
+    for wl in wait_list:
+        getReceipt(wl)
 
 import networkx
 power_graph = networkx.barabasi_albert_graph(N, 7)
@@ -108,10 +118,11 @@ wait_list = []
 #     for j in range(i, N):
 #         edge = (i, j)
 for edge in power_graph.edges:
-        print(edge)
-        wait_list.append(createAccTransaction(sort_contract, edge[0], edge[1]))
-for wl in wait_list:
-    getReceipt(wl)
+        # print(edge)
+        wait_list.append(createAccTransaction(sort_contract, edge[0], edge[1], gr=grcpt))
+if not grcpt:
+    for wl in wait_list:
+        getReceipt(wl)
 
 getSucCountCall(sort_contract)
 
@@ -122,10 +133,11 @@ while (t<T):
     if (sender==recvr):
         continue
     t += 1
-    wait_list.append(sendAmountTransaction(sort_contract, sender, recvr, 1))
+    wait_list.append(sendAmountTransaction(sort_contract, sender, recvr, 1, gr=grcpt))
     if (t%interval==0):
-        for wl in wait_list:
-            getReceipt(wl)
+        if not grcpt:
+            for wl in wait_list:
+                getReceipt(wl)
         wait_list = []
         getSucCountCall(sort_contract)
         print("Number of Total Transactions:", t)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 765_a3
+1. Make sure you have the softwares in requirements.txt
+
+2. Run the following command and copy the address to the genesis.json in the alloc section that adds the balance to the geth account.
+
+geth --datadir $HOME/765_a3/test-eth1/ --password $HOME/765_a3/password.txt account new
+
+3. Run the following command to set up the Ethereum node.
+
+sh runEthereumNodeMy.sh
+
+4. In a parallel terminal run the following.
+
+bash run.sh
